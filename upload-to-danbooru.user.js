e646992668f6769bd831911c1697525219f71564
hdk5 <hdk5@hdk5.xyz>
2023-10-30 15:41:10 +0200

    Bump userscript version

--- upload-to-danbooru.user.js
+++ upload-to-danbooru.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Upload To Danbooru
 // @author       hdk5
-// @version      20231025162442
+// @version      20231030134050
 // @description  another userscript for uploading to danbooru
 // @homepage     https://github.com/hdk5/upload-to-danbooru.user.js
 // @homepageURL  https://github.com/hdk5/upload-to-danbooru.user.js
